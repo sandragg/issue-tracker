d20efeaddc322240a341efe17956c621913e4ffd
sandragg <sandragg@yandex.ru>
2019-01-31 01:14:47 +0300

    Created authorization on server

---
+++ backend/services/authorize.js
@@ -0,0 +1,18 @@
+const { User } = require('../models').models;
+
+module.exports = (req, res) => {
+    const { login, password } = req.body;
+
+    if (!login || !password) {
+        return res.sendStatus(400);
+    }
+
+    User.findByPk(login)
+        .then(user => {
+            if (user && user.password === password) {
+                return res.send({ id: user.id, name: user.name });
+            }
+            res.status(403).send('Wrong login or password');
+        })
+        .catch(err => res.status(500).send(err))
+};
