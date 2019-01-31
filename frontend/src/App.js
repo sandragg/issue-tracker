c859373b512ea3d8c2e917eb2d674e8698e32e41
sandragg <sandragg@yandex.ru>
2019-01-31 20:39:14 +0300

    Added Header and changed paths in App component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,22 +6,29 @@ import {
     Switch
 } from 'react-router-dom';
 import * as Pages from './pages';
+import { Header } from "./components/header";
 import { Session } from "./services/session";
+import { ROUTES } from "./services/routes";
 import './App.css';
 
 export const App = () => (
     <BrowserRouter>
-        <div>
-            <Switch>
-                <Redirect exact from="/" to="/list/issue" />
-                <Route exact path="/login" component={Pages.AuthorizePage} />
-                <Route path="/" render={
-                    () => Session.isAuthorized() ? null : <Redirect to="/login" />
-                } />
-            </Switch>
-            <Route exact path="/create/:type" component={Pages.EditorPage} />
-            <Route exact path="/edit/:id" component={Pages.EditorPage} />
-            <Route exact path="/list/:type" component={Pages.ListPage} />
+        <div className="page">
+            <Header />
+            <main>
+                <Switch>
+                    <Redirect exact from="/" to={ROUTES.HOME} />
+                    <Route exact path={ROUTES.AUTHORIZE} component={Pages.AuthorizePage} />
+                    <Route path="/" render={
+                        () => !Session.isAuthorized()
+                            ? <Redirect to={ROUTES.AUTHORIZE} />
+                            : null
+                    } />
+                </Switch>
+                <Route exact path={ROUTES.CREATE} component={Pages.EditorPage} />
+                <Route exact path={ROUTES.EDIT} component={Pages.EditorPage} />
+                <Route exact path={ROUTES.LIST} component={Pages.ListPage} />
+            </main>
         </div>
     </BrowserRouter>
 );
