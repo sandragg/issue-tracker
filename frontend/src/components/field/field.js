c621a8b2e7a2047014ec87af453f69cd00544952
sandragg <sandragg@yandex.ru>
2019-02-03 18:17:14 +0300

    Created Field component

---
+++ frontend/src/components/field/field.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import './field.css';
+
+export const Field = props => {
+	const { id, name, type } = props;
+	const Component = inputs[type];
+
+	return (
+        <div className="field">
+            <label
+				className="field__label"
+				htmlFor={id}
+			>
+				{name}
+			</label>
+            <Component {...props} />
+        </div>
+	);
+};
+
+const Input = ({ name, ...props }) => (
+    <input
+        className="field__input"
+        placeholder={`Enter the ${name}`}
+        {...props}
+    />
+);
+
+const SelectInput = ({ options, ...props }) => (
+    <select
+		className="field__input"
+        {...props}
+	>
+		{options && options.map(({ id, name }) => (
+			<option key={id} value={id}>{name}</option>
+		))}
+    </select>
+);
+
+const inputs = {
+	'text': Input,
+	'select': SelectInput
+};
