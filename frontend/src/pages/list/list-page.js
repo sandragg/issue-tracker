bf3e45ccec9fd84041338688252fc67bc03487eb
sandragg <sandragg@yandex.ru>
2019-02-04 13:39:58 +0300

    Small fix in ListPage component

--- frontend/src/pages/list/list-page.js
+++ frontend/src/pages/list/list-page.js
@@ -3,36 +3,35 @@ import React, {
     useState,
     useEffect
 } from 'react';
-import { ROUTES } from 'services/routes';
 import { listsOptions } from 'services/list-options';
 import { Field } from 'api/models';
 import { Table } from 'components/table';
 import './list-page.css';
 
 export const ListPage = (props) => {
-    const { match: { params }} = props;
+    const { params: { type }} = props.match;
     const [ list, setList ] = useState(null);
     const listOpts = useRef(null);
     const listFields = useRef(null);
 
     useEffect(() => {
-        listOpts.current = listsOptions[params.type];
+        listOpts.current = listsOptions[type];
 
         Promise.all([
             listOpts.current.model.getAll(),
-            Field.getById(params.type)
+            Field.getById(type)
         ])
             .then(([ items, fields ]) => {
                 listFields.current = fields.filter(filterVisibleFields);
                 setList(items);
             });
-    }, [params.type]);
+    }, [type]);
 
     return list && list.length ? (
         <section className="section list-section">
             <Table
                 list={list}
-                listType={params.type}
+                listType={type}
                 fields={listFields.current}
                 idField={listOpts.current.idField}
             />
